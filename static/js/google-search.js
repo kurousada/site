f341009242619a6f78a2cd3f67a7bf1570934008
Kuro Usada <kurousada+account@gmail.com>
2017-06-15 03:03:36 +0900

    add google search

---
+++ static/js/google-search.js
@@ -0,0 +1,9 @@
+(function() {
+  var cx = '018175984642811849393:lior-73d8wc';
+  var gcse = document.createElement('script');
+  gcse.type = 'text/javascript';
+  gcse.async = true;
+  gcse.src = 'https://cse.google.com/cse.js?cx=' + cx;
+  var s = document.getElementsByTagName('script')[0];
+  s.parentNode.insertBefore(gcse, s);
+})();
